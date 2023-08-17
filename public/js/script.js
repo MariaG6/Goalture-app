593d4eeb3c83b92ddc571f65cce2432a7dcdc772
Maria Garcia <mariagarciamontes06@outlook.com>
2023-08-17 22:12:45 +0200

    Fixed bugs

--- public/js/script.js
+++ public/js/script.js
@@ -6,37 +6,40 @@ document.addEventListener("DOMContentLoaded", () => {
 // Create more steps at the creategoal form
 const steps = [];
 const stepsForm = document.getElementById("steps-form");
-let index = 0
+let index = 0;
 stepsForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target); // get the values from the form and pass it as an object
   const fromEntries = Object.fromEntries(formData); // get the object and create key-value pairs
   steps.push(fromEntries); // add to steps
-  index++
-  drawSteps(fromEntries); 
+  index++;
+  drawSteps(fromEntries);
 });
 
 const drawSteps = (fromEntries) => {
-  const stepContainer = document.querySelector("#step-container"); 
-    const step = fromEntries;
-    const stepHtml = generateStepHtml(step,index);
-    stepContainer.appendChild(stepHtml);
+  const stepContainer = document.querySelector("#step-container");
+  const step = fromEntries;
+  const stepHtml = generateStepHtml(step, index);
+  stepContainer.appendChild(stepHtml);
 };
 
 const generateStepHtml = (stepData, index) => {
-  console.log(stepData,index)
+  console.log(stepData, index);
   const stepLi = document.createElement("li");
-  const currentStep = `step${index-1}`
-  const currentBlocker = `blockers${index-1}`
+  const currentStep = `step${index - 1}`;
+  const currentBlocker = `blockers${index - 1}`;
   stepLi.innerHTML = `
-    <input type="text" name="step${index}" value=" " />
-    Do you have any blockers?
-    <input type="text" name="blockers${index}" value=" " /><br />
-    <label for="isCompleted">Done</label>
+    <input type="text"  class="form-control" name="step${index}" value=" " />
+    <label class="form-label">Do you have any blockers?</label>
+    <input type="text" class="form-control" name="blockers${index}" value=" " />
+    <div class="form-check">
     <input type="checkbox" name="isCompleted${index}" ${
-    stepData.isCompleted ? "checked" : ""
-  } /><br />
+    stepData.isCompleted ? "checked" : ""} />
+    <label for="isCompleted">Done</label>
+     </div>
   `;
+  stepLi.className = 'list-group-item'
+  console.log(stepLi)
   return stepLi;
 };
 
@@ -54,17 +57,17 @@ const addNewStep = () => {
 drawSteps();
 
 // This is to display the Edit form when the Edit button is clicked
-const editButton = document.getElementById('edit-goal'); 
-const editForm = document.getElementById('edit-form'); 
-editButton.addEventListener('click', () => { 
-  console.log('click')
-  editForm.style.display = 'block'; 
+const editButton = document.getElementById("edit-goal");
+const editForm = document.getElementById("edit-form");
+editButton.addEventListener("click", () => {
+  console.log("click");
+  editForm.style.display = "block";
 });
 
-const deleteButton = document.getElementById('delete-goal');
-const deleteForm = document.getElementById('delete-form');
-deleteButton.addEventListener('click', () => {
-  const confirmed = confirm('Are you sure you want to delete this goal?');
+const deleteButton = document.getElementById("delete-goal");
+const deleteForm = document.getElementById("delete-form");
+deleteButton.addEventListener("click", () => {
+  const confirmed = confirm("Are you sure you want to delete this goal?");
   if (confirmed) {
     deleteForm.submit();
   }
